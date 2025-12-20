112b884b51c17b9f95b4d2843f593b31455415ed
bala-amypo <bala@amypo.in>
2025-12-20 12:37:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/InteractionCheckResult.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public class InteractionCheckResult{
+    private Long id;
+    private String medications;
+    private String interaction;
+    prvaie LocalDateTime checkedAt;
+    
+}
