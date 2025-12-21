8148ad07ab98890c4581d5468c835dd52e5390ef
bala-amypo <bala@amypo.in>
2025-12-21 13:12:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,32 +1,32 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
-
 @RestController
-public class AuthController{
-@Autowired UserService userService;
+public class AuthController {
 
-    @PostMapping("/register")
-    public User register(@RequestBody User user){
-        return userService.registerUser(user);
+    @Autowired
+    private UserService userService;
 
-    }
-@PostMapping("/login")
-public String login(@RequestBody User user){
-    User dbUser=userService.findByEmail(user.getEmail());
-
-    id(dbUser != null && dbUser.getPassword().equals(user.getPassword())){
-        return "Login successful";
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
     }
-    else{
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+
+        User dbUser = userService.findByEmail(user.getEmail());
+
+        if (dbUser != null && dbUser.getPassword().equals(user.getPassword())) {
+            return "Login Successful";
+        }
         return "Invalid Credentials";
     }
-}
 }
