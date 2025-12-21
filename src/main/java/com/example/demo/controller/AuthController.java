1a9e6918cd07e551fadd267b1d88a1d635966fca
bala-amypo <bala@amypo.in>
2025-12-21 15:25:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,32 +1,32 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
+// import com.example.demo.model.User;
+// import com.example.demo.service.UserService;
 
-@RestController
-public class AuthController {
+// @RestController
+// public class AuthController {
 
-    @Autowired
-    private UserService userService;
+//     @Autowired
+//     private UserService userService;
 
-    @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return userService.registerUser(user);
-    }
+//     @PostMapping("/register")
+//     public User register(@RequestBody User user) {
+//         return userService.registerUser(user);
+//     }
 
-    @PostMapping("/login")
-    public String login(@RequestBody User user) {
+//     @PostMapping("/login")
+//     public String login(@RequestBody User user) {
 
-        User dbUser = userService.findByEmail(user.getEmail());
+//         User dbUser = userService.findByEmail(user.getEmail());
 
-        if (dbUser != null && dbUser.getPassword().equals(user.getPassword())) {
-            return "Login Successful";
-        }
-        return "Invalid Credentials";
-    }
-}
+//         if (dbUser != null && dbUser.getPassword().equals(user.getPassword())) {
+//             return "Login Successful";
+//         }
+//         return "Invalid Credentials";
+//     }
+// }
