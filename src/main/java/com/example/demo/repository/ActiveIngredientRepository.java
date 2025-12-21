edc9c6835896387a4e14448673a16468d67e5d06
bala-amypo <bala@amypo.in>
2025-12-21 16:25:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/ActiveIngredientRepository.java
+++ src/main/java/com/example/demo/repository/ActiveIngredientRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.User;
+import com.example.demo.model.ActiveIngredient;
 
 @Repository
 
