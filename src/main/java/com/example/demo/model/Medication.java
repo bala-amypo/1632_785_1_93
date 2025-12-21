17ebd91dd686136566d4715ace21934de158fe22
bala-amypo <bala@amypo.in>
2025-12-21 15:36:53 +0000

    User commit

--- src/main/java/com/example/demo/model/Medication.java
+++ src/main/java/com/example/demo/model/Medication.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 
 import jakarta.persistence.Entity;
 @Entity
