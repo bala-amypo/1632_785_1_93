4cb0b6e97377a8c747f2545306552d10c5a27bd1
bala-amypo <bala@amypo.in>
2025-12-21 14:49:23 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -1,22 +1,22 @@
-// package com.example.demo.model;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import lombok.Data;
-// import lombok.AllArgsConstructor;
-// import lombok.NoArgsConstructor;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// @Entity
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
+package com.example.demo.model;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
-// public class ActiveIngredient{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     @Column(name="name" unique = true)
-//     private String name;  
+public class ActiveIngredient{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;  
 
-// }
+}
