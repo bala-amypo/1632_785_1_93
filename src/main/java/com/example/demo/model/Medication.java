571b42a0f53c47703392d3c4df8c4fbbfc90c2cb
bala-amypo <bala@amypo.in>
2025-12-21 15:49:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Medication.java
+++ src/main/java/com/example/demo/model/Medication.java
@@ -1,40 +1,40 @@
-package com.example.demo.model;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import jakarta.persistence.Table;
-import java.util.HashSet;
-import java.util.Set;
+// package com.example.demo.model;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import lombok.Data;
+// import lombok.AllArgsConstructor;
+// import lombok.NoArgsConstructor;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Table;
+// import java.util.HashSet;
+// import java.util.Set;
 
-import jakarta.persistence.Entity;
-@Entity
-@Table(name="medications")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+// import jakarta.persistence.Entity;
+// @Entity
+// @Table(name="medications")
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
 
-public class Medication{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    @Column(name= "ingredient_name")
-    private Set<String> ingredients = new HashSet<>();
+// public class Medication{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+//     private String name;
+//     @Column(name= "ingredient_name")
+//     private Set<String> ingredients = new HashSet<>();
 
-    public Medication(String name){
-        this.name=name;
-        this.ingredients=new HashSet<>();
-    }
+//     public Medication(String name){
+//         this.name=name;
+//         this.ingredients=new HashSet<>();
+//     }
 
-    public void addIngredient(String ingredient){
-        this.ingredients.add(ingredient);
-    }
-     public void removeIngredient(String ingredient){
-        this.ingredients.remove(ingredient);
-    }
+//     public void addIngredient(String ingredient){
+//         this.ingredients.add(ingredient);
+//     }
+//      public void removeIngredient(String ingredient){
+//         this.ingredients.remove(ingredient);
+//     }
 
-}
+// }
