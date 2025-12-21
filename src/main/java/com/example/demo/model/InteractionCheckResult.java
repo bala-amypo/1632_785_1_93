278087c64dc7ebfa1e822adcc19d933cf8815c1b
bala-amypo <bala@amypo.in>
2025-12-21 19:34:24 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionCheckResult.java
+++ src/main/java/com/example/demo/model/InteractionCheckResult.java
@@ -21,6 +21,25 @@ public class InteractionCheckResult{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String medications;
+    [INFO] 2 errors 
+[INFO] -------------------------------------------------------------
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD FAILURE
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  31.098 s
+[INFO] Finished at: 2025-12-21T19:31:52Z
+[INFO] ------------------------------------------------------------------------
+[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.13.0:compile (default-compile) on project demo: Compilation failure: Compilation failure: 
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/model/InteractionCheckResult.java:[14,12] ')' expected
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/model/InteractionCheckResult.java:[14,13] class, interface, enum, or record expected
+[ERROR] -> [Help 1]
+[ERROR] 
+[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
+[ERROR] Re-run Maven using the -X switch to enable full debug logging.
+[ERROR] 
+[ERROR] For more information about the errors and possible solutions, please read the following articles:
+[ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
+coder@9227448ac6cb:~/Workspace/demo$ 
     private String interaction;
     private LocalDateTime checkedAt;
 
