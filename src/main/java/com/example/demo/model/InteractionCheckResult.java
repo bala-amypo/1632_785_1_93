8d6d8bcc7deeb274f1253b2240a748cfe0d325d4
bala-amypo <bala@amypo.in>
2025-12-21 17:19:24 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionCheckResult.java
+++ src/main/java/com/example/demo/model/InteractionCheckResult.java
@@ -1,24 +1,26 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import lombok.Data;
-// import lombok.AllArgsConstructor;
-// import lombok.NoArgsConstructor;
-// import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+@Entity
+@Table(name "interaction_check_results")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
-// import jakarta.persistence.Entity;
-// @Entity
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
+public class InteractionCheckResult{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String medications;
+    private String interaction;
+    private LocalDateTime checkedAt;
 
-// public class InteractionCheckResult{
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     private String medications;
-//     private String interaction;
-//     private LocalDateTime checkedAt;
-
-// }
+}
