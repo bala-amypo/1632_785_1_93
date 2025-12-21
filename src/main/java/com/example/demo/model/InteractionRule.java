9e6b7b55ccd42959f1080c72551e6d2e9a8c6cd3
bala-amypo <bala@amypo.in>
2025-12-21 17:07:24 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionRule.java
+++ src/main/java/com/example/demo/model/InteractionRule.java
@@ -1,26 +1,28 @@
-// package com.example.demo.model;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import lombok.Data;
-// import lombok.AllArgsConstructor;
-// import lombok.NoArgsConstructor;
-// import jakarta.persistence.Id;
+package com.example.demo.model;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-// import jakarta.persistence.Entity;
-// @Entity
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
+import jakarta.persistence.Entity;
+@Entity
+@Table(name = "interaction_rules")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
-// public class InteractionRule{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     //-----
+public class InteractionRule{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    //-----
 
-//     private String severity;
-//     private String description;
+    private String severity;
+    private String description;
     
     
 
-// }
+}
