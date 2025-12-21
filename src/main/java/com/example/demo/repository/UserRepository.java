ce708fa386b98edef1f70aca5e22058da9d23b17
bala-amypo <bala@amypo.in>
2025-12-21 16:48:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,14 +1,14 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
-// import com.example.demo.model.User;
+import com.example.demo.model.User;
 
-// @Repository
+@Repository
 
-// public interface UserRepository extends JpaRepository<User,Long>{
-//     User findByEmail(String email);
+public interface UserRepository extends JpaRepository<User,Long>{
+    User findByEmail(String email);
 
-// }
+}
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,31 +1,31 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import com.example.demo.model.User;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.service.UserService;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 
-// @Service
-// public class UserServiceImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService {
 
-//     @Autowired
-//     private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-//     @Override
-//     public User registerUser(User user) {
+    @Override
+    public User registerUser(User user) {
 
-//         // Default role assignment
-//         if (user.getRole() == null) {
-//             user.setRole("USER");
-//         }
+        // Default role assignment
+        if (user.getRole() == null) {
+            user.setRole("USER");
+        }
 
-//         return userRepository.save(user);
-//     }
+        return userRepository.save(user);
+    }
 
-//     @Override
-//     public User findByEmail(String email) {
-//         return userRepository.findByEmail(email);
-//     }
-// }
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
