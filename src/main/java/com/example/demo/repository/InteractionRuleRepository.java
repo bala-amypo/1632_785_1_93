3d7fa496e585d1a4a45f4de02d31d4c40f90feb8
bala-amypo <bala@amypo.in>
2025-12-22 08:39:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/InteractionRuleRepository.java
+++ src/main/java/com/example/demo/repository/InteractionRuleRepository.java
@@ -1,11 +1,11 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import com.example.demo.model.InteractionRule;
+import com.example.demo.model.InteractionRule;
 
-// public interface InteractionRuleRepository extends JpaRepository<InteractionRule, Long> {
-//         List<InteractionRule> findByIngredientId(Long ingredientId);
-// }
+public interface InteractionRuleRepository extends JpaRepository<InteractionRule, Long> {
+        List<InteractionRule> findByIngredientId(Long ingredientId);
+}
--- src/main/java/com/example/demo/service/impl/InteractionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InteractionServiceImpl.java
@@ -1,22 +1,22 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import com.example.demo.model.InteractionCheckResult;
-// import com.example.demo.service.InteractionService;
+import com.example.demo.model.InteractionCheckResult;
+import com.example.demo.service.InteractionService;
 
-// @Service
-// public class InteractionServiceImpl implements InteractionService {
+@Service
+public class InteractionServiceImpl implements InteractionService {
 
-//     @Override
-//     public InteractionCheckResult checkInteractions(List<Long> medicationIds) {
-//         return null;
-//     }
+    @Override
+    public InteractionCheckResult checkInteractions(List<Long> medicationIds) {
+        return null;
+    }
 
-//     @Override
-//     public InteractionCheckResult getResult(Long resultId) {
-//         return null;
-//     }
-// }
+    @Override
+    public InteractionCheckResult getResult(Long resultId) {
+        return null;
+    }
+}
