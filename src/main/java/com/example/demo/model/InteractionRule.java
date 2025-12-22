95ed7b5454bb9679a516e9acc81c4c94005cac3e
bala-amypo <bala@amypo.in>
2025-12-22 08:38:53 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionRule.java
+++ src/main/java/com/example/demo/model/InteractionRule.java
@@ -1,32 +1,32 @@
-// package com.example.demo.model;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import lombok.Data;
-// import lombok.AllArgsConstructor;
-// import lombok.NoArgsConstructor;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
+package com.example.demo.model;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-// import jakarta.persistence.Entity;
-// @Entity
-// @Table(name = "interaction_rules")
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
+import jakarta.persistence.Entity;
+@Entity
+@Table(name = "interaction_rules")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
-// public class InteractionRule{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     private String ingredientA;
-//     private String ingredientB;
-//     private String severity;
-//     private String description;
+public class InteractionRule{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String ingredientA;
+    private String ingredientB;
+    private String severity;
+    private String description;
 
-//     public InteractionRule(String ingredientA,String ingredientB,String severity, String description) {
-//     this.ingredientA = ingredientA;
-//     this.ingredientB = ingredientB;
-//     this.severity = severity;
-//     this.description = description;
-//  }
-// }
+    public InteractionRule(String ingredientA,String ingredientB,String severity, String description) {
+    this.ingredientA = ingredientA;
+    this.ingredientB = ingredientB;
+    this.severity = severity;
+    this.description = description;
+ }
+}
