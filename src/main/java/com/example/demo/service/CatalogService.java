83ac77457be4ddf04c467ab2b4def958f7f17023
bala-amypo <bala@amypo.in>
2025-12-22 04:39:44 +0000

    User commit

--- src/main/java/com/example/demo/service/CatalogService.java
+++ src/main/java/com/example/demo/service/CatalogService.java
@@ -1,13 +1,13 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import java.util.List;
+// import java.util.List;
 
-import com.example.demo.model.ActiveIngredient;
-import com.example.demo.model.Medication;
+// import com.example.demo.model.ActiveIngredient;
+// import com.example.demo.model.Medication;
 
-public interface CatalogService {
+// public interface CatalogService {
     
-    ActiveIngredient addIngredient(ActiveIngredient ingredient);
-    Medication addMedication(Medication medication);
-    List<Medication> getAllMedications();
-}
+//     ActiveIngredient addIngredient(ActiveIngredient ingredient);
+//     Medication addMedication(Medication medication);
+//     List<Medication> getAllMedications();
+// }
