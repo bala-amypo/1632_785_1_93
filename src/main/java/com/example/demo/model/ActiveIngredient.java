2d9b9bc76c0eeed41cba66825876fac9eec4f545
bala-amypo <bala@amypo.in>
2025-12-23 09:44:33 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.MantToMany;
 @Entity
 @Table(name="active_ingredients")
 @Data
