49703360a31cf52e26a2f67ab403aef03411aa73
bala-amypo <bala@amypo.in>
2025-12-23 06:46:05 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -25,3 +25,32 @@ public class ActiveIngredient{
         this.name=name;
     }
 }
+
+
+// package com.example.demo.model;
+
+// import java.util.Set;
+
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.ManyToMany;
+
+// @Entity
+// @Table(name = "active_ingredients")
+// public class ActiveIngredient {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     @Column(unique = true)
+//     private String name;
+
+//     // Many-to-many with Medication
+//     @ManyToMany(mappedBy = "ingredients")
+//     private Set<Medication> medications;
+// }
