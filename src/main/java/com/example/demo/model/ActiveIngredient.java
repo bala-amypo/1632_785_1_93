5efc094e470d45746a7acce5c68cd2fdd2a03bca
bala-amypo <bala@amypo.in>
2025-12-23 10:01:56 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -1,33 +1,32 @@
 package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-//import jakarta.persistence.MantToMany;
+
 @Entity
-@Table(name="active_ingredients")
+@Table(name = "active_ingredients")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+public class ActiveIngredient {
 
-public class ActiveIngredient{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
-    private String name;  
+    private String name;
 
-    public ActiveIngredient(String name){
-        this.name=name;
+    
+    public ActiveIngredient(String name) {
+        this.name = name;
     }
-
-    //@ManyToMany(mappedBy = "ingredients")
-    List<InteractionRule> findByIngredientA_Id(Long ingredientId);
-
 }
-
