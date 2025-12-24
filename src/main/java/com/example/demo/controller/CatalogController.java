fbe9fd3738ee0c8a5583250aef0ed18f22ca11de
bala-amypo <bala@amypo.in>
2025-12-24 17:41:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/CatalogController.java
+++ src/main/java/com/example/demo/controller/CatalogController.java
@@ -1,41 +1,41 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.example.demo.model.ActiveIngredient;
-// import com.example.demo.model.Medication;
-// import com.example.demo.service.CatalogService;
+import com.example.demo.model.ActiveIngredient;
+import com.example.demo.model.Medication;
+import com.example.demo.service.CatalogService;
 
-// @RestController
-// public class CatalogController {
+@RestController
+public class CatalogController {
 
-//     private final CatalogService catalogService;
+    private final CatalogService catalogService;
 
-//     public CatalogController(CatalogService catalogService) {
-//         this.catalogService = catalogService;
-//     }
+    public CatalogController(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
 
-//     @PostMapping("/catalog/ingredient")
-//     public ActiveIngredient addIngredient(
-//             @RequestBody ActiveIngredient ingredient) {
-//         return catalogService.addIngredient(ingredient);
-//     }
+    @PostMapping("/catalog/ingredient")
+    public ActiveIngredient addIngredient(
+            @RequestBody ActiveIngredient ingredient) {
+        return catalogService.addIngredient(ingredient);
+    }
 
     
-//     @PostMapping("/catalog/medication")
-//     public Medication addMedication(
-//             @RequestBody Medication medication) {
-//         return catalogService.addMedication(medication);
-//     }
+    @PostMapping("/catalog/medication")
+    public Medication addMedication(
+            @RequestBody Medication medication) {
+        return catalogService.addMedication(medication);
+    }
 
 
-//     @GetMapping("/catalog/medications")
-//     public List<Medication> getAllMedications() {
-//         return catalogService.getAllMedications();
-//     }
-// }
+    @GetMapping("/catalog/medications")
+    public List<Medication> getAllMedications() {
+        return catalogService.getAllMedications();
+    }
+}
