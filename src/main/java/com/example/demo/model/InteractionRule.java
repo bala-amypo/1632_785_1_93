0b8218c4b6632cb7b2d1c7167bce74bd1a6e6145
bala-amypo <bala@amypo.in>
2025-12-25 14:34:57 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionRule.java
+++ src/main/java/com/example/demo/model/InteractionRule.java
@@ -1,29 +1,69 @@
+// package com.example.demo.model;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import lombok.Data;
+// import lombok.AllArgsConstructor;
+// import lombok.NoArgsConstructor;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.ManyToOne;
+
+// @Entity
+// @Table(name = "interaction_rules")
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+
+// public class InteractionRule{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+//     @ManyToOne
+//     private ActiveIngredient ingredientA;
+//     private ActiveIngredient ingredientB;
+//     private String severity;
+//     private String description;
+
+   
+// }
+
+
 package com.example.demo.model;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "interaction_rules")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+public class InteractionRule {
 
-public class InteractionRule{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     private ActiveIngredient ingredientA;
+
+    @ManyToOne
     private ActiveIngredient ingredientB;
+
     private String severity;
     private String description;
 
-   
+    public InteractionRule() {}
+
+    public InteractionRule(ActiveIngredient ingredientA,
+                           ActiveIngredient ingredientB,
+                           String severity,
+                           String description) {
+        this.ingredientA = ingredientA;
+        this.ingredientB = ingredientB;
+        this.severity = severity;
+        this.description = description;
+    }
 }
