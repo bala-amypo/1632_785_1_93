bc0bf93d397e6ea5ec7b87bdd96088a9c95e6c38
bala-amypo <bala@amypo.in>
2025-12-25 11:46:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/RuleServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.impl;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.InteractionRule;
+import com.example.demo.model.ActiveIngredient;
+import com.example.demo.repository.InteractionRuleRepository;
+import com.example.demo.service.RuleService;
+
+@Service
+public class RuleServiceImpl implements RuleService {
+
+    private final InteractionRuleRepository ruleRepository;
+
+    public RuleServiceImpl(InteractionRuleRepository ruleRepository) {
+        this.ruleRepository = ruleRepository;
+    }
+
+@Override
+public InteractionRule addRule(InteractionRule rule) {
+if (!rule.getSeverity().equals("MINOR") &&!rule.getSeverity().equals("MODERATE") &&
+!rule.getSeverity().equals("MAJOR")) {
+throw new IllegalArgumentException("Invalid severity");
+    }
+
+     ActiveIngredient a = rule.getIngredientA();
+     ActiveIngredient b = rule.getIngredientB();
+
+    Optional<InteractionRule> ruleAB =ruleRepository.findByIngredientAAndIngredientB(a, b);
+    Optional<InteractionRule> ruleBA =ruleRepository.findByIngredientAAndIngredientB(b, a);
+     if (ruleAB.isPresent() || ruleBA.isPresent()) {
+        throw new IllegalArgumentException("Ingredient pairing already exists");
+    }
+    return ruleRepository.save(rule);
+}
+
+    @Override
+    public List<InteractionRule> getAllRules() {
+        return ruleRepository.findAll();
+    }
+}
