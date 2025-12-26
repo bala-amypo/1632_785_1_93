1950bc8ec488d29b75ad1c1f95996b65db167bdc
bala-amypo <bala@amypo.in>
2025-12-26 16:27:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -20,9 +20,14 @@ public class UserServiceImpl implements UserService {
         }
         return userRepository.save(user);
     }
-
-    @Override
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
+@Override
+public User findByEmail(String email) {
+    return userRepository
+            .findByEmail(email)
+            .orElseThrow(() -> new RuntimeException("User not found"));
 }
+}
+
+
+
+
