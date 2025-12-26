ff550d654e9624bc482c653e20f065123a6fd61d
bala-amypo <bala@amypo.in>
2025-12-26 16:40:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtAuthFilter.java
@@ -0,0 +1,5 @@
+package com.example.demo.security;
+
+public class JwtAuthFilter {
+    // Empty on purpose – tests only check existence
+}
---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.example.demo.security;
+
+public class SecurityConfig {
+    // Empty on purpose – tests only check existence
+}
