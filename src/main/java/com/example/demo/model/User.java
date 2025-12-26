d8bdfcf4aa6725fb0566fff12e49f17ad815ac89
bala-amypo <bala@amypo.in>
2025-12-26 14:01:05 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,3 +1,37 @@
+// package com.example.demo.model;
+
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Column;
+
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+// import lombok.AllArgsConstructor;
+
+// @Entity
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class User {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     private String name;
+
+//     @Column(unique = true)
+//     private String email;
+
+//     private String password;
+
+//     private String role;
+// }
+
+
+
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
@@ -8,12 +42,10 @@ import jakarta.persistence.Column;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -27,5 +59,13 @@ public class User {
 
     private String password;
 
-    private String role;
+    private String role = "USER";
+
+  
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = "USER";
+    }
 }
