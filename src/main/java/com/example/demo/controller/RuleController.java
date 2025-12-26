5844fe54f22c7bcd71e4fc6ba8d4827cf64ca09f
bala-amypo <bala@amypo.in>
2025-12-26 14:44:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/RuleController.java
+++ src/main/java/com/example/demo/controller/RuleController.java
@@ -1,31 +1,31 @@
-package com.example.demo.controller;
-import java.util.List;
+// package com.example.demo.controller;
+// import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.InteractionRule;
-import com.example.demo.service.RuleService;
+// import com.example.demo.model.InteractionRule;
+// import com.example.demo.service.RuleService;
 
-@RestController
-public class RuleController {
+// @RestController
+// public class RuleController {
 
-    private final RuleService ruleService;
+//     private final RuleService ruleService;
 
-    public RuleController(RuleService ruleService) {
-        this.ruleService = ruleService;
-    }
+//     public RuleController(RuleService ruleService) {
+//         this.ruleService = ruleService;
+//     }
 
-    @PostMapping("/rules")
-    public InteractionRule addRule(
-            @RequestBody InteractionRule rule) {
-        return ruleService.addRule(rule);
-    }
+//     @PostMapping("/rules")
+//     public InteractionRule addRule(
+//             @RequestBody InteractionRule rule) {
+//         return ruleService.addRule(rule);
+//     }
 
-    @GetMapping("/rules")
-    public List<InteractionRule> getAllRules() {
-        return ruleService.getAllRules();
-    }
-}
+//     @GetMapping("/rules")
+//     public List<InteractionRule> getAllRules() {
+//         return ruleService.getAllRules();
+//     }
+// }
